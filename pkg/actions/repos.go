f0d33cf6848c3f074a48925627a7d62263c2811c
Marcin Chalczynski <m.chalczynski@gmail.com>
2021-09-21 16:56:37 +0200

    action: actually implement top repos by watch events
    pretty naive implementation:
    - gather WatchEvent stats in a map, keyed by repo_id (from events.csv)
    - translate the map to a slice
    - sort slice, get top 10 elems by count
    - only then - try to find names for the small result set by digging into
    repos.csv

--- pkg/actions/repos.go
+++ pkg/actions/repos.go
@@ -1,11 +1,23 @@
 package actions
 
 import (
+	"sort"
+
 	"github.com/mchalski/gitstat/pkg/data"
 )
 
 const (
 	topDefault = 10
+
+	// tuple field constants
+	evtTypeWatch = "WatchEvent"
+
+	// tuple field indices
+	evtIdxType   = 1
+	evtIdxRepoId = 3
+
+	repoIdxId   = 0
+	repoIdxName = 1
 )
 
 // TopReposByWatchEvts represents listing the top repos by the number of watch events
@@ -46,8 +58,86 @@ func NewTopReposByCommits(evts data.Stream, commits data.Stream, repos data.Stre
 }
 
 func (a *TopReposByWatchEvts) Run() ([]RepoRes, error) {
-	//TODO
-	return nil, nil
+	// map repo ids to repo results
+	cntMap := make(map[string]RepoRes)
+
+	// count watch events in the 'events' stream
+	evtChan := a.evts.C()
+	for e := range evtChan {
+		if e.Err != nil {
+			return nil, e.Err
+		}
+
+		evtType, repoId := e.Tuple[evtIdxType], e.Tuple[evtIdxRepoId]
+
+		if evtType == evtTypeWatch {
+			repo, ok := cntMap[repoId]
+			if !ok {
+				repo = RepoRes{
+					Id:    repoId,
+					Count: 0,
+				}
+			}
+
+			repo.Count += 1
+			cntMap[repoId] = repo
+		}
+	}
+
+	// pull repo results into a slice, sort, get top
+	res := make([]RepoRes, len(cntMap))
+
+	i := 0
+	for _, v := range cntMap {
+		res[i] = v
+		i += 1
+	}
+
+	sort.Slice(res, func(i, j int) bool {
+		return res[i].Count > res[j].Count
+	})
+
+	topIdx := topDefault
+	if topIdx > len(res) {
+		topIdx = len(res)
+	}
+
+	res = res[:topIdx]
+
+	// find repo names in the 'repos' stream
+	repoChan := a.repos.C()
+	for e := range repoChan {
+		if e.Err != nil {
+			return nil, e.Err
+		}
+
+		repoId, repoName := e.Tuple[repoIdxId], e.Tuple[repoIdxName]
+
+		// finish early if we have all top names
+		if fillName(repoId, repoName, res) {
+			break
+		}
+	}
+
+	return res, nil
+}
+
+// fillName tries to match/insert a name into a set of repo results described by ids so far
+// returns a flag telling if all repos have names already set
+func fillName(id, name string, repos []RepoRes) bool {
+	allFilled := true
+
+	for i, r := range repos {
+		if r.Name == "" {
+			allFilled = false
+		}
+
+		if r.Id == id {
+			repos[i].Name = name
+		}
+	}
+
+	return allFilled
 }
 
 func (a *TopReposByCommits) Run() ([]RepoRes, error) {

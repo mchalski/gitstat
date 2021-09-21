33acf10ec56ba92c2011bcf85e24952a7f57b3cb
Marcin Chalczynski <m.chalczynski@gmail.com>
2021-09-21 16:56:39 +0200

    action: implement top repos by number of commits
    similarily pretty naive implementation to the previous one:
    - gather partial commit counts per push event
    - roll them up using a count map
    - translate the map to slice, sort, get top 10

--- pkg/actions/repos.go
+++ pkg/actions/repos.go
@@ -11,13 +11,17 @@ const (
 
 	// tuple field constants
 	evtTypeWatch = "WatchEvent"
+	evtTypePush  = "PushEvent"
 
 	// tuple field indices
+	evtIdxId     = 0
 	evtIdxType   = 1
 	evtIdxRepoId = 3
 
 	repoIdxId   = 0
 	repoIdxName = 1
+
+	commitIdxEvtId = 2
 )
 
 // TopReposByWatchEvts represents listing the top repos by the number of watch events
@@ -141,6 +145,109 @@ func fillName(id, name string, repos []RepoRes) bool {
 }
 
 func (a *TopReposByCommits) Run() ([]RepoRes, error) {
-	//TODO
-	return nil, nil
+	// map event ids to repo results
+	// (counts correspond to # commits per event initially)
+	evtCntMap := make(map[string]RepoRes)
+
+	// record push events
+	evtChan := a.evts.C()
+	for e := range evtChan {
+		if e.Err != nil {
+			return nil, e.Err
+		}
+
+		evtId, evtType, repoId := e.Tuple[evtIdxId],
+			e.Tuple[evtIdxType],
+			e.Tuple[evtIdxRepoId]
+
+		if evtType == evtTypePush {
+			_, ok := evtCntMap[repoId]
+
+			if !ok {
+				repo := RepoRes{
+					Id:    repoId,
+					Count: 0,
+				}
+				evtCntMap[evtId] = repo
+			}
+		}
+	}
+
+	// count commits for each event
+	commitChan := a.commits.C()
+	for e := range commitChan {
+		if e.Err != nil {
+			return nil, e.Err
+		}
+
+		evtId := e.Tuple[commitIdxEvtId]
+
+		// if we're interested in this event - it should already
+		// be tracked, bump the commit counter if so
+		res, ok := evtCntMap[evtId]
+		if !ok {
+			continue
+		}
+
+		res.Count += 1
+		evtCntMap[evtId] = res
+	}
+
+	// at this point - we have a slice of partial repo counts
+	// need to sum them up; use a count map again
+
+	// map repo ids to repo results
+	cntMap := make(map[string]RepoRes)
+
+	for _, r := range evtCntMap {
+		repo, ok := cntMap[r.Id]
+		if !ok {
+			repo = RepoRes{
+				Id:    r.Id,
+				Count: 0,
+			}
+		}
+
+		// bump the final counter by the partial count
+		repo.Count += r.Count
+		cntMap[repo.Id] = repo
+
+	}
+
+	// pull repo results into a slice, sort, get top
+	res := make([]RepoRes, len(cntMap))
+
+	i := 0
+	for _, v := range cntMap {
+		res[i] = v
+		i += 1
+	}
+
+	sort.Slice(res, func(i, j int) bool {
+		return res[i].Count > res[j].Count
+	})
+
+	topIdx := topDefault
+	if topIdx > len(res) {
+		topIdx = len(res)
+	}
+
+	res = res[:topIdx]
+
+	// find repo names in the 'repos' stream
+	repoChan := a.repos.C()
+	for e := range repoChan {
+		if e.Err != nil {
+			return nil, e.Err
+		}
+
+		repoId, repoName := e.Tuple[repoIdxId], e.Tuple[repoIdxName]
+
+		// finish early if we have all top names
+		if fillName(repoId, repoName, res) {
+			break
+		}
+	}
+
+	return res, nil
 }
